c1778b986d5765ba163388da0ab9fb7f5e35c6f9
Melvin Tercan <mmtercan@gmail.com>
2009-11-26 14:44:39 +0100

    fixed namespacing bug

--- libraries/MY_Database.php
+++ libraries/MY_Database.php
@@ -4,19 +4,129 @@ class Database extends Database_Core {
 	
 	public $result;
 	
-	public function __construct(){
+	public function __construct($config = array()){
 		
 		parent::__construct();
 		
+		if (empty($config))
+		{
+			// Load the default group
+			$config = Kohana::config('database.default');
+		}
+		elseif (is_array($config) AND count($config) > 0)
+		{
+			if ( ! array_key_exists('connection', $config))
+			{
+				$config = array('connection' => $config);
+			}
+		}
+		elseif (is_string($config))
+		{
+			// The config is a DSN string
+			if (strpos($config, '://') !== FALSE)
+			{
+				$config = array('connection' => $config);
+			}
+			// The config is a group name
+			else
+			{
+				$name = $config;
+		
+				// Test the config group name
+				if (($config = Kohana::config('database.'.$config)) === NULL)
+					throw new Kohana_Database_Exception('database.undefined_group', $name);
+			}
+		}
+		
+		// Merge the default config with the passed config
+		$this->config = array_merge($this->config, $config);
+		
+		if (is_string($this->config['connection']))
+		{
+			// Make sure the connection is valid
+			if (strpos($this->config['connection'], '://') === FALSE)
+				throw new Kohana_Database_Exception('database.invalid_dsn', $this->config['connection']);
+		
+			// Parse the DSN, creating an array to hold the connection parameters
+			$db = array
+			(
+				'type'     => FALSE,
+				'user'     => FALSE,
+				'pass'     => FALSE,
+				'host'     => FALSE,
+				'port'     => FALSE,
+				'socket'   => FALSE,
+				'database' => FALSE
+			);
+		
+			// Get the protocol and arguments
+			list ($db['type'], $connection) = explode('://', $this->config['connection'], 2);
+		
+			if (strpos($connection, '@') !== FALSE)
+			{
+				// Get the username and password
+				list ($db['pass'], $connection) = explode('@', $connection, 2);
+				// Check if a password is supplied
+				$logindata = explode(':', $db['pass'], 2);
+				$db['pass'] = (count($logindata) > 1) ? $logindata[1] : '';
+				$db['user'] = $logindata[0];
+		
+				// Prepare for finding the database
+				$connection = explode('/', $connection);
+		
+				// Find the database name
+				$db['database'] = array_pop($connection);
+		
+				// Reset connection string
+				$connection = implode('/', $connection);
+		
+				// Find the socket
+				if (preg_match('/^unix\([^)]++\)/', $connection))
+				{
+					// This one is a little hairy: we explode based on the end of
+					// the socket, removing the 'unix(' from the connection string
+					list ($db['socket'], $connection) = explode(')', substr($connection, 5), 2);
+				}
+				elseif (strpos($connection, ':') !== FALSE)
+				{
+					// Fetch the host and port name
+					list ($db['host'], $db['port']) = explode(':', $connection, 2);
+				}
+				else
+				{
+					$db['host'] = $connection;
+				}
+			}
+			else
+			{
+				// File connection
+				$connection = explode('/', $connection);
+		
+				// Find database file name
+				$db['database'] = array_pop($connection);
+		
+				// Find database directory name
+				$db['socket'] = implode('/', $connection).'/';
+			}
+		
+			// Reset the connection array to the database config
+			$this->config['connection'] = $db;
+		}
 		// Set driver name
 		$driver = 'Flex_'.ucfirst($this->config['connection']['type']).'_Driver';
-
-		// Load the driver
+		
+		// Load the driver		// 
 		if ( ! Kohana::auto_load($driver))
 			throw new Kohana_Database_Exception('core.driver_not_found', $this->config['connection']['type'], get_class($this));
-
+		
 		// Initialize the driver
 		$this->driver = new $driver($this->config);
+		
+		// Validate the driver
+		if ( ! ($this->driver instanceof Database_Driver))
+			throw new Kohana_Database_Exception('core.driver_implements', $this->config['connection']['type'], get_class($this), 'Database_Driver');
+		
+		Kohana::log('debug', 'Database Library initialized');
 		
 	}
 	
---
+++ libraries/drivers/Flex/Mysqli.php
@@ -0,0 +1,158 @@
+<?php
+
+class Flex_Mysqli_Driver extends Database_Mysqli_Driver{
+	
+	public function __construct($config){
+		
+		parent::__construct($config);
+		
+	}
+	
+	public function query($sql)
+	{
+		// Only cache if it's turned on, and only cache if it's not a write statement
+		if ($this->db_config['cache'] AND ! preg_match('#\b(?:INSERT|UPDATE|REPLACE|SET|DELETE|TRUNCATE)\b#i', $sql))
+		{
+			$hash = $this->query_hash($sql);
+
+			if ( ! isset($this->query_cache[$hash]))
+			{
+				// Set the cached object
+				$this->query_cache[$hash] = new Flex_Mysqli_Result(mysql_query($sql, $this->link), $this->link, $this->db_config['object'], $sql);
+			}
+			else
+			{
+				// Rewind cached result
+				$this->query_cache[$hash]->rewind();
+			}
+
+			// Return the cached query
+			return $this->query_cache[$hash];
+		}
+
+		return new Flex_Mysqli_Result(mysql_query($sql, $this->link), $this->link, $this->db_config['object'], $sql);
+	}	
+	
+}
+
+class Flex_Mysqli_Result extends Mysqli_Result{
+	
+	public function __construct($result, $link, $object = TRUE, $sql){
+		parent::__construct($result, $link, $object = TRUE, $sql);	
+	}
+	
+	public function as_array(){
+		
+		$array = $this->real_array();
+		
+		if(count($array) > 0){
+			
+			return $array;
+			
+		}else{
+			
+			return false;
+		}
+	}
+
+	
+	public function single_row(){
+				
+		$array = $this->real_array();
+		
+		if(isset($array[0])){
+			
+			return $array[0];
+			
+		}else{
+			
+			return false;
+		}
+	}
+	
+	public function single_value(){
+		
+		$array = $this->real_array(false);
+		
+		if(isset($array[0]) && is_array($array[0]) && count($array[0]) > 0){
+			
+			return reset($array[0]); 
+			
+		}else{
+			
+			return false;
+		}
+		
+	}
+	
+	public function real_array(){
+		
+		$num_fields = mysql_num_fields($this->result);
+		
+		$rows = $this->result_array(false);
+		
+		if ($num_fields > 0 && count($rows) > 0)
+		{
+						
+			for ($i=0; $i < $num_fields; $i++) {
+				
+				$fields[mysql_field_name($this->result, $i)] = array(mysql_field_type($this->result, $i), mysql_field_len($this->result, $i));							
+			}
+			
+			foreach($rows as &$array){
+				
+				foreach ($array as $field_name => &$value){
+					
+					switch($fields[$field_name][0]){
+						
+						case "int":
+							
+							if($fields[$field_name][1] === 1){
+								
+								if($value == 1){
+									
+									$value = true;
+									
+								}else{
+									
+									$value = false;
+								}	
+							}else{
+						
+								$value = intval($value);
+							}
+							
+						break;
+						
+						case "real":
+						
+							$value = floatval($value);
+							
+						break;
+						
+					}
+					
+				}
+				
+			}	
+			
+			return $rows;	
+		}
+	}
+	
+	public function success(){
+
+		if($this->count() > 0){
+			
+			return true;
+			
+		}else{
+			
+			return false;
+		}
+		
+	}
+	
+
+	
+}
